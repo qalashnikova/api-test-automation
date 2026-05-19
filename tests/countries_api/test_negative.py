e10d1a146a4315bfc1fae4a7df0117249cff140a
qalashnikova <with.wan.hong@gmail.com>
2026-05-19 10:03:58 +0700

    Add negative tests for invalid country, code, currency, region, language

--- tests/countries_api/test_negative.py
+++ tests/countries_api/test_negative.py
@@ -15,3 +15,55 @@ class TestInvalidCountry:
     def test_message(self, response):
         data = response.json()
         assert data["message"] == "Not Found"
+
+
+class TestInvalidCode:
+    @pytest.fixture
+    def response(self):
+        return requests.get(f"{BASE_URL}/alpha/wat")
+
+    def test_status_code(self, response):
+        assert response.status_code == 404
+
+    def test_message(self, response):
+        data = response.json()
+        assert data["message"] == "Not Found"
+
+
+class TestInvalidCurrency:
+    @pytest.fixture
+    def response(self):
+        return requests.get(f"{BASE_URL}/currency/auf")
+
+    def test_status_code(self, response):
+        assert response.status_code == 404
+
+    def test_message(self, response):
+        data = response.json()
+        assert data["message"] == "Not Found"
+
+
+class TestInvalidRegion:
+    @pytest.fixture
+    def response(self):
+        return requests.get(f"{BASE_URL}/region/mars")
+
+    def test_status_code(self, response):
+        assert response.status_code == 404
+
+    def test_message(self, response):
+        data = response.json()
+        assert data["message"] == "Not Found"
+
+
+class TestInvalidLanguage:
+    @pytest.fixture
+    def response(self):
+        return requests.get(f"{BASE_URL}/lang/elf")
+
+    def test_status_code(self, response):
+        assert response.status_code == 404
+
+    def test_message(self, response):
+        data = response.json()
+        assert data["message"] == "Not Found"
