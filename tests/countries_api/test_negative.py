d8daf2a1fbf3a9346cde22c37120d3e521249457
qalashnikova <with.wan.hong@gmail.com>
2026-05-19 10:14:59 +0700

    Refactor negative tests with parametrize

--- tests/countries_api/test_negative.py
+++ tests/countries_api/test_negative.py
@@ -3,67 +3,11 @@ import pytest
 from config import BASE_URL
 
 
-class TestInvalidCountry:
-
-    @pytest.fixture
-    def response(self):
-        return requests.get(f"{BASE_URL}/name/utopia")
-
-    def test_status_code(self, response):
-        assert response.status_code == 404
-
-    def test_message(self, response):
-        data = response.json()
-        assert data["message"] == "Not Found"
-
-
-class TestInvalidCode:
-    @pytest.fixture
-    def response(self):
-        return requests.get(f"{BASE_URL}/alpha/wat")
-
-    def test_status_code(self, response):
-        assert response.status_code == 404
-
-    def test_message(self, response):
-        data = response.json()
-        assert data["message"] == "Not Found"
-
-
-class TestInvalidCurrency:
-    @pytest.fixture
-    def response(self):
-        return requests.get(f"{BASE_URL}/currency/auf")
-
-    def test_status_code(self, response):
-        assert response.status_code == 404
-
-    def test_message(self, response):
-        data = response.json()
-        assert data["message"] == "Not Found"
-
-
-class TestInvalidRegion:
-    @pytest.fixture
-    def response(self):
-        return requests.get(f"{BASE_URL}/region/mars")
-
-    def test_status_code(self, response):
-        assert response.status_code == 404
-
-    def test_message(self, response):
-        data = response.json()
-        assert data["message"] == "Not Found"
-
-
-class TestInvalidLanguage:
-    @pytest.fixture
-    def response(self):
-        return requests.get(f"{BASE_URL}/lang/elf")
-
-    def test_status_code(self, response):
-        assert response.status_code == 404
-
-    def test_message(self, response):
-        data = response.json()
-        assert data["message"] == "Not Found"
+@pytest.mark.parametrize(
+    "url", ["/name/utopia", "/alpha/wat", "/currency/auf", "/region/mars", "/lang/elf"]
+)
+def test_invalid_returns_404(url):
+    response = requests.get(f"{BASE_URL}{url}")
+    data = response.json()
+    assert response.status_code == 404
+    assert data["message"] == "Not Found"
