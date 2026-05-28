a2e06ccd2dbb199ede2ce9cf1a489b7a7be7b1b2
qalashnikova <with.wan.hong@gmail.com>
2026-05-28 12:02:31 +0700

    add reqres config with env variable support

--- tests/config.py
+++ tests/config.py
@@ -1,1 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_URL = "https://restcountries.com/v3.1"
+REQRES_BASE_URL = "https://reqres.in/api"
+REQRES_API_KEY = os.getenv("REQRES_API_KEY")
